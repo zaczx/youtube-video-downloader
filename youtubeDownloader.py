ea7ff1f643adfb370dfefbee3a17a55cef1041b9
zaczx <amroot@pm.me>
2020-02-15 01:22:47 +0300

    initial commit on youtube video downloader

---
+++ youtubeDownloader.py
@@ -0,0 +1,21 @@
+from pytube import YouTube
+from bs4 import BeautifulSoup
+import requests
+
+
+search = "https://www.youtube.com/results"
+
+video_search = input("Type the name of the video: ")
+params ={"search_query": video_search}
+
+r = requests.get(search, params=params, verify= False)
+soup = BeautifulSoup(r.text, 'html.parser')
+result = soup.find('a',{'aria-hidden': 'true'})
+link = result['href']
+
+yt = YouTube('http://www.youtube.com' + link)
+
+first_video = yt.streams.filter(progressive=True, subtype = 'mp4').first()
+
+print(first_video.title)
+
